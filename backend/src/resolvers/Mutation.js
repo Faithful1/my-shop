c4a82ba2637eb166622af865abce6d3826028373
logicboy <faithfulanere@gmail.com>
2019-03-01 19:31:24 +0800

    my first mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const Mutations = {};
+const Mutations = {
+    createItems(parent, args, ctx, info) {
+        // create an item
+        global.items = global.items || [];
+
+        const newItem =  { name: args.name };
+        global.items.push(newItem);
+        return newItem;
+    }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+// when there is a new query you must set a resolver
+// answers the question where does this query come from 
+// and how to get it to the users
+const Query = {
+    items(parent, args, ctx, info) {
+        global.items = global.items || [];
+
+        return global.items;
+    },
+};
 
 module.exports = Query;
