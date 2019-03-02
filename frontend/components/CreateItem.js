315953ea63ffc11e65a358a1bd0774d8878321ee
logicboy <faithfulanere@gmail.com>
2019-03-02 18:51:41 +0800

    upload images

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -42,6 +42,25 @@ handleChange = e => {
     this.setState({ [name]: val });
 };
 
+uploadFile = async e => {
+    console.log('uploading file')
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'faya-shop');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/fayatech/image/upload', {
+        method: 'POST',
+        body: data
+    });
+    const file = await res.json()
+    console.log(file)
+    this.setState({
+        image: file.secure_url,
+        largeImage:file.eager[0].secure_url
+    });
+}
+
 // for mutation it takes two args (mutationfunction, payload)
 // used implicit return to return data for mutation
 render() {
@@ -65,6 +84,21 @@ render() {
             >
                 <Error error={error}/>
                 <fieldset disabled={loading} aria-busy={loading}>
+                <label htmlFor="file">
+                        Image
+                        <input 
+                            type="file" 
+                            id="file" 
+                            name="file"
+                            placeholder="Upload an image" 
+                            // required value={this.state.image}
+                            onChange={this.uploadFile}
+                        />
+                        {this.state.image && 
+                            <img src={this.state.image} 
+                            alt="Upload preview"
+                            width="200"/>}
+                    </label>
                     <label htmlFor="title">
                         Title
                         <input 
