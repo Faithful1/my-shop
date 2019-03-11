918f9ed7d0c8bade03edc7a503ee5b0765d67d2c
logicboy <faithfulanere@gmail.com>
2019-03-11 14:12:16 +0800

    user signup and permission flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 // now interface with the prisma database/api (everything inside prisma.graphql)
 const Mutations = {
   async createItem (parent, args, ctx, info) {
@@ -17,15 +20,35 @@ const Mutations = {
       info
     )
   },
-  async deleteItem(parent, args, ctx, info) {
-    const where = {id: args.id};
-    //1. find the Item
-    const item = await ctx.db.query.item({ where }, `{id title}`);
-    //2. check if they own that item, or have the permissions
-    // TODO:
-    //3. delete it
-    return ctx.db.mutation.deleteItem({ where }, info);
+  async deleteItem (parent, args, ctx, info) {
+    const where = { id: args.id }
+    // 1. find the Item
+    const item = await ctx.db.query.item({ where }, `{id title}`)
+    // TODO: 2. check if they own that item, or have the permissions
+
+    // 3. delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signUp (parent, args, ctx, info) {
+    // lowercase all emails
+    args.email = args.email.toLowerCase()
+    // hash their passwords (pass it a salt of 10)
+    const password = await bcrypt.hash(args.password, 10)
+    // create user in the database
+    const user = await ctx.db.mutation.createUser(
+      { data: { ...args, password, permissions: { set: [ 'USER' ] } } },
+      info
+    );
+    // create the jwt token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set the jwt as a cookie on the response
+    ctx.response.cookie( 'token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 , // 1 year cookie
+    });
+    // we return the user to the browser
+    return user;
   }
-};
+}
 
 module.exports = Mutations
